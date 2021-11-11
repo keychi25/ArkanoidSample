047bffc4179a512cc10fdfd393c5319c60648add
keychi25 <m208125n@st.u-gkugei.ac.jp>
2021-11-11 11:21:05 +0900

    finish: boxのプレハブ化

--- Assets/Scripts/BallStart.cs
+++ Assets/Scripts/BallStart.cs
@@ -7,7 +7,9 @@ public class BallStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // スクリプトがアタッチされているオブジェクトのTransformの回転角情報をx=0，y=ランダムで(30~120)，z=0にする．
         transform.eulerAngles = new Vector3(0, Random.Range(30, 120), 0);
+        // このスクリプトがアタッチされているオブジェクトにアタッチされている"RigidBody"コンポーネントのAddForce関数を呼ぶ．
         gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
     }
 
